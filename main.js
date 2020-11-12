1e9a65ee2a49ff14e3d378cd9e16ae0d5b40862d
TheSmoothy <72082606+YaItzYoBoiX@users.noreply.github.com>
2020-11-12 14:29:57 +0800

    Main File
    Added Main File. Note that the file name was "main.js" and not "index.js".

---
+++ main.js
@@ -0,0 +1,71 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+const { token } = require('./config.json');
+
+const { readdirSync } = require('fs');
+
+const { join } = require('path');
+
+const config = require('./config.json');
+client.config = config;
+
+const { GiveawaysManager } = require('discord-giveaways')
+
+client.GiveawaysManager = new GiveawaysManager(client, {
+    storage: "./giveaways.json",
+    updateCountdownEvery: 5000,
+    default: {
+        botsCanWin: false,
+        exemptPermissions: ["MANAGE_MESSAGES", "ADMINISTRATOR"],
+        embedColor: "#ff0000",
+        reaction: "🎉"
+    }
+});
+
+client.commands= new Discord.Collection();
+
+const prefix = '+';
+//You can change the prefix if you like. It doesn't have to be ! or ;
+
+
+const commandFiles = readdirSync(join(__dirname, "commands")).filter(file => file.endsWith(".js"));
+
+for (const file of commandFiles) {
+    const command = require(join(__dirname, "commands", `${file}`));
+    client.commands.set(command.name, command);
+}
+
+
+client.on("error", console.error);
+
+client.on('ready', () => {
+    console.log('Your bot is ready.');
+    client.user.setActivity('+help | version: 1.0.12')
+});
+
+
+client.on("message", async message => {
+
+    if(message.author.bot) return;
+    if(message.channel.type === 'dm') return;
+
+    if(message.content.startsWith(prefix)) {
+        const args = message.content.slice(prefix.length).trim().split(/ +/);
+
+        const command = args.shift().toLowerCase();
+
+        if(!client.commands.has(command)) return;
+
+
+        try {
+            client.commands.get(command).run(client, message, args);
+
+        } catch (error){
+            console.error(error);
+        }
+    }
+})
+
+client.login(token);
