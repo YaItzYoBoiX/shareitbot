1041bf69a97095080fb0f4ae12f3ae2e68014c8c
TheSmoothy <72082606+YaItzYoBoiX@users.noreply.github.com>
2020-11-21 19:06:29 +0800

    Add files via upload

---
+++ blueprints.js
@@ -0,0 +1,9 @@
+module.exports = {
+    name: "blueprints",
+    description: "Bleh",
+
+   async run (client, message, args) {
+
+       message.channel.send('```Blueprints (Beta Development)\r\n1.Crew Dragon Falcon 9\r\n2.Falcon Heavy\r\n3.NASA SLS Orbital\r\n4.Starship SPACEX```');
+   }
+}
